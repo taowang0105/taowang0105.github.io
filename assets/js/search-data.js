2062fee1728fb7cccae44ca8a3001ba9283926a0
taowang0105 <76874921+taowang0105@users.noreply.github.com>
2025-12-10 14:00:51 +0000

    Deploying to gh-pages from @ taowang0105/taowang0105.github.io@c62cefeac8492c0ce43301574ebb7e77513b23d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,8 +391,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-win-the-huawei-challenge-solving-114th-spark-award-as-a-member-of-dr-cheng-meng-s-team",
-          title: 'I win the Huawei “Challenge Solving” 114th Spark Award as a member of...',
+            },},{id: "news-i-won-the-huawei-challenge-solving-114th-spark-award-as-a-member-of-dr-cheng-meng-s-team",
+          title: 'I won the Huawei “Challenge Solving” 114th Spark Award as a member of...',
           description: "",
           section: "News",},{id: "news-i-awarded-an-outstanding-poster-award-at-the-9th-student-forum-of-the-china-society-for-industrial-and-applied-mathematics-csiam-️",
           title: 'I awarded an Outstanding Poster Award at the 9th Student Forum of the...',
@@ -446,46 +446,32 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%77%61%6E%67_%74%61%6F@%72%75%63.%65%64%75.%63%6E", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/https://github.com/taowang0105", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://orcid.org/https://orcid.org/0009-0004-4584-6285", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=https://scholar.google.com/citations?user=rMnOvOUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
